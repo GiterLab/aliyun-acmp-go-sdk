01d3ff863a92c87a5f89276ac61a75e2527be370
MWangxj <wangxianjin028@hotmail.com>
2017-10-22 17:11:19 +0800

    add the push message method

--- acmp/push/pushmessage.go
+++ acmp/push/pushmessage.go
@@ -1,13 +1,38 @@
 package push
 
-func PushMessage() {
-	pushMessgaeToAndroid()
-	pushMessageToiOS()
-}
+import (
+	"errors"
+	"net/http"
+	"fmt"
+	"aliyun-acmp-go-sdk/acmp/bean"
+	"aliyun-acmp-go-sdk/acmp/signature"
+)
 
-func pushMessgaeToAndroid() {
-
-}
-func pushMessageToiOS() {
-
+func PushMessage(rootUrl,accessSecret *string,publicParam *bean.PublicParam,noticeParam *bean.MessageParam)(resp *http.Response,err error) {
+	if publicParam==nil||noticeParam==nil {
+		return nil, errors.New("PushMessage param pointer shouldn't be nil")
+	}
+	publicParamStr,_:=publicParam.ToStringWithoutSignature()
+	noticeParamStr,_:=noticeParam.ToString()
+	urlstr:=*rootUrl+"/?"+*publicParamStr+*noticeParamStr
+	var httpurlp *string
+	httpurlp=&urlstr
+	method:=http.MethodGet
+	var httpMethodP *string
+	httpMethodP=&method
+	signstr,err:=signature.SignatureString(httpurlp,httpMethodP)
+	if err!=nil {
+		return nil,errors.New("PushMessage signature.SignatureString err")
+	}
+	signaturestr,err:=signature.GetSignature(signstr,accessSecret)
+	if err!=nil {
+		return nil,errors.New("PushMessage signature.GetSignature err")
+	}
+	finalUrlStr:=urlstr+"&Signature="+*signaturestr
+	fmt.Println(finalUrlStr)
+	resp,err=http.Get(finalUrlStr)
+	if err!=nil {
+		return nil,errors.New("PushMessage http.Get err")
+	}
+	return resp,nil
 }
